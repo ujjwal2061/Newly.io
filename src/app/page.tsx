865ccd1b51943c2decb92d2a9ee58d65195be9d2
neyuj_11 <164717360+ujjwal2061@users.noreply.github.com>
2025-06-24 16:26:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/sections/Hero-section";
 import Middel from "@/components/sections/Middel-section";
 import Price from "@/components/sections/Price";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
